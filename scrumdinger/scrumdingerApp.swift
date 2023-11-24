22dddd65659824d6547e1081d2085fe951f270ef
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-11-24 12:55:25 +0700

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by yazidal on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
