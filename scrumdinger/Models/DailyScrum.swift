e2ff41721c3a952575ff47ebd0f3d6221de890b9
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-11-26 15:59:43 +0700

    new detail view

--- scrumdinger/Models/DailyScrum.swift
+++ scrumdinger/Models/DailyScrum.swift
@@ -10,25 +10,35 @@ import Foundation
 struct DailyScrum {
     let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthOfMinutes: Int
     var theme: Theme
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthOfMinutes: Int, theme: Theme) {
         self.id = id
         self.title = title
-        self.attendees = attendees
+        self.attendees = attendees.map { Attendee(name: $0 )}
         self.lengthOfMinutes = lengthOfMinutes
         self.theme = theme
     }
 }
 
-
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id : UUID
+        var name : String
+        
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
+}
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
         DailyScrum(title: "Design",
-                   attendees: ["Yazid", "Melati", "Fahzan", "Lia"],
+                   attendees: ["Yazid Al", "Aurelia Melati", "Fahzan", "Lia Suci"],
                    lengthOfMinutes: 10,
                    theme: .yellow),
         DailyScrum(title: "App Dev",
